c8f619c01cce70a18f9a40dac96353601397c35d
CGGrimsley <144144388+CGGrimsley@users.noreply.github.com>
2023-11-08 09:42:27 -0500

    Comments updated
    - Comments shortened/removed

--- dist/main.py
+++ dist/main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import subprocess
 
-# Function to launch the selected application
+# Choose the application wanted
 def launch_selected_app():
     selected_app = app_combobox.get()
     if selected_app == "Sulfur Calculator":
@@ -10,11 +10,11 @@ def launch_selected_app():
     elif selected_app == "Raid Cost Calculator":
         subprocess.Popen(["python", "raidcost.py"])  # Replace with the actual script name
 
-# Create the main window
+# Create window
 root = tk.Tk()
 root.title("Choose an Application")
 
-# Create a label
+# Label
 ttk.Label(root, text="Choose an Application:").pack(padx=10, pady=10)
 
 # Create a combobox to select the application
@@ -22,8 +22,7 @@ app_combobox = ttk.Combobox(root, values=["Sulfur Calculator", "Raid Cost Calcul
 app_combobox.pack(padx=10, pady=10)
 app_combobox.set("Sulfur Calculator")  # Default selection
 
-# Create a button to launch the selected application
+# Create a button to launch application
 ttk.Button(root, text="Launch", command=launch_selected_app).pack(padx=10, pady=10)
 
-# Run the GUI
 root.mainloop()
